c60c31b73a6131d8c3ce3d600cbc20c4269c2e80
alex <alexander@henss.net>
2022-03-11 23:18:53 +0100

    added missing domain to asys url

--- asys/urls.py
+++ asys/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.UserCreate.as_view(), name='account-create'),
-    path('login/', views.CustomAuthToken.as_view(), name='account-login'),
-    path('test/', views.ExampleView.as_view(), name='test'),
+    path('<domainname>/users/', views.UserCreate.as_view(), name='account-create'),
+    path('<domainname>/login/', views.CustomAuthToken.as_view(), name='account-login'),
+    path('<domainname>/test/', views.ExampleView.as_view(), name='test'),
     path('<domainname>/appointment/', views.AppointmentsView.as_view(), name='appointments'),
     path('<domainname>/appointmentpublic/', views.AppointmentsPublicView.as_view(), name='appointments'),
 ]
